10db79beb4d85188888640bb8cbb1a2f0ed37ee5
Ajith T Alias <61543419+fullstack-alpha@users.noreply.github.com>
2020-11-30 10:41:37 +0530

    adding docker,healthckeck,swagger

--- src/main/java/com/customer/config/SecurityConfig.java
+++ src/main/java/com/customer/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,9 +27,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	http.csrf().disable()
-         .authorizeRequests().anyRequest().authenticated()
-         .and().httpBasic();
-      
+         .authorizeRequests()
+         .anyRequest().authenticated()
+         .and().httpBasic(); 	
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+          .ignoring()
+            .antMatchers("/actuator/**");
     }
     
     @Bean
